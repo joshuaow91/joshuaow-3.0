df2088baf99f4aeaf1e2a8663d3597c7e4e9b9cf
Joshua Ow <joshuaow@gmail.com>
2023-07-27 21:19:25 -0500

    hero section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
+
 @Component ({
     templateUrl: './header.component.html',
     selector: 'app-header'
@@ -10,4 +11,5 @@ export class HeaderComponent {
     faTwitter = faTwitter
     faLinkedin = faLinkedin
     faGithub = faGithub
+    
 }
