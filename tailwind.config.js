7ab95d40513435d424ca6e602d0996be74e2d6fd
Joshua Ow <joshuaow@gmail.com>
2023-07-28 10:35:28 -0500

    working on hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('src/assets/grad.jpeg')",
+      }
+    },
   },
   plugins: [],
 }
