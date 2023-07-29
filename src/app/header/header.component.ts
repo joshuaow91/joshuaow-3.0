8880fd9be93f44a6a19a3bc7a051f2fdd598e027
Joshua Ow <joshuaow@gmail.com>
2023-07-29 12:18:32 -0500

    ui

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,53 @@
-import { Component } from "@angular/core";
-
+import { Component, HostListener, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Scroll } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component ({
     templateUrl: './header.component.html',
     selector: 'app-header'
 })
-
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+    constructor(private router: Router, private route: ActivatedRoute) {}
+  
+    ngOnInit() {
+        this.router.events.pipe(
+          // filter out events that are not of type Scroll
+          filter(e => e instanceof Scroll)
+        ).subscribe((e: any) => {
+          // if fragment exists in the URL
+          if (e.anchor) {
+            // wait for the DOM to be rendered
+            setTimeout(() => {
+              // get the element with id matching the fragment
+              const element = document.querySelector('#' + e.anchor);
+              if (element) {
+                // get the position of the element relative to the top of the document
+                const rect = element.getBoundingClientRect();
+                const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                const elementTop = rect.top + scrollTop;
+                
+                // scroll to the element
+                window.scrollTo({
+                  top: elementTop,
+                  behavior: 'smooth'
+                });
+              }
+            }, 0);
+          }
+        });
+      }
+    isHidden = false;
+    lastScrollTop = 0;
     
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+        if (currentScrollTop > 300) {
+          this.isHidden = currentScrollTop > this.lastScrollTop;
+        }
+        else {
+          this.isHidden = false;
+        }
+        this.lastScrollTop = currentScrollTop;
+    }
 }
