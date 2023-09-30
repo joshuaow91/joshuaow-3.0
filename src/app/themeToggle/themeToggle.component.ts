7d606c4d6b92b051daabdeb7e7bf7f96dee606b3
Joshua Ow <joshuaow@gmail.com>
2023-09-30 10:49:02 -0500

    some ui changes

--- src/app/themeToggle/themeToggle.component.ts
+++ src/app/themeToggle/themeToggle.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../services/theme.service';
+
 @Component({
   selector: 'app-toggle',
   template: `
-    <!-- <button (click)="toggleTheme()">
-      {{ themeService.isDarkMode() ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}
-    </button> -->
-
     <label for="toogleDark2" aria-label="toggle light or dark mode for project preview" class="flex items-center cursor-pointer">
         <div class="relative">
-            <input id="toogleDark2" type="checkbox" class="sr-only" (click)="toggleTheme()" />
+            <input id="toogleDark2" type="checkbox" class="sr-only" (click)="toggleTheme()" [checked]="themeService.isDarkMode()" />
             <div class="block bg-blue-400 w-14 h-8 rounded-full"></div>
             <div class="absolute left-1 top-1 bg-zinc-50 w-6 h-6 rounded-full transition-transform duration-300 ease-in-out" [ngClass]="{'transform translate-x-full': themeService.isDarkMode()}"></div>
         </div>
     </label>
   `,
 })
-export class ToggleComponent {
+export class ToggleComponent implements OnInit {
 
   constructor(public themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    const storedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null;
+    if (storedTheme === 'light' || storedTheme === 'dark') {
+      this.themeService.update(storedTheme);
+      return;
+    }
+    
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.themeService.update('dark');
+    } else {
+      this.themeService.update('light');
+    }
+}
+
 
   toggleTheme() {
     if (this.themeService.isDarkMode()) {
